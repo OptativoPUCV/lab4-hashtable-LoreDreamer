6c1a020d83b734351af4a12f8be621192640a34e
LoreDreamer <55300635+LoreDreamer@users.noreply.github.com>
2024-04-23 14:59:15 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -153,7 +153,18 @@ Pair * searchMap(HashMap * map, char * key) {
 
 Pair * firstMap(HashMap * map) {
 
+  if (map == NULL || key == NULL) 
     return NULL;
+
+  for (long i = 0; i < map->capacity; i++) {
+
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+
+      map->current = i;
+      return map->buckets[i];
+      
+    }
+  }
 }
 
 Pair * nextMap(HashMap * map) {
