8fd90ffc8f14b1938319e2fdd88fb4a99b47a893
LoreDreamer <55300635+LoreDreamer@users.noreply.github.com>
2024-04-23 14:19:35 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,10 +54,11 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
 
     HashMap * map = (HashMap *) malloc(sizeof(HashMap));
+  
     map->buckets = (Pair **) calloc(capacity, sizeof(Pair *));
     map->capacity = capacity;
     map->size = 0;
-    map->current = -1;
+    map->current = 0;
     return map;
   
 }
